fcc34d9df7552795a2aee5cdf550c1eeab2769ec
testing <vanshikagarg249177@gmail.com>
2024-03-25 13:34:17 +0530

    url commit

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,4 @@
-const dbUrl="mongodb://127.0.0.1:27017/trainees";
+// const dbUrl="mongodb://127.0.0.1:27017/trainees";
+const dbUrl="mongodb+srv://vanshika24:admin123@cluster0.zo9qjzo.mongodb.net/trainees?retryWrites=true&w=majority"
 
 module.exports={dbUrl};
